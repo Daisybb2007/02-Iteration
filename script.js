2f1b24e729efba35e50a70c77433ad3461be0527
daisy <daisybb545@gmail.com>
2024-05-04 08:56:47 +0630

    Iteration

---
+++ script.js
@@ -0,0 +1,24 @@
+var country = ['Korea', 'Japan', 'Thailand', 'Myanmar', 'Singapore'];
+console.log(country.length);
+for (var i=0; i<country.length; i++){
+    console.log('I want to go' + ' '+ country[i]+'!');
+}
+
+
+// While Loop
+let n=0;
+while(n < 11){
+    console.log(n);
+    n++
+}
+var letters = ["A","5","M","0","Z","2"];
+for (letter of letters){
+    console.log(letter);
+}
+
+
+var S = 0;
+while(S<5){
+    console.log("This number is "+ S);
+    S=S+1
+}
